019f728f72abe9f5ffb40024da74fea41c7b8492
Eden Andom <andomheden@gmail.com>
2024-06-21 17:36:23 +0000

    actually last one

--- calc.py
+++ calc.py
@@ -20,7 +20,7 @@ class Calc:
     def pow(self, a, b):
         """Raise a to the power of b"""
         return a ** b
-    
+
     def sqrt(self, a):
         """Return the square root of a"""
         return a ** 0.5
